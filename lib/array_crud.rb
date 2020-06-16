19a278804fa3da92942bd2b4c53f9cd7837b78ac
Adam Workman <adammworkman25@gmail.com>
2020-06-16 22:49:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,6 @@ end
 
 def add_element_to_end_of_array(array, element)
   array.push(element)
- array.index(2)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -26,7 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.index(2)
 end
 
 def retrieve_first_element_from_array(array)
